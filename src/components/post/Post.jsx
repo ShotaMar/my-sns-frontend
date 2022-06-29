f8c647d751e9dadef03cbcc4a424700a8bb91559
ShotaMar <nu05kyntobronk@gmail.com>
2022-06-29 23:47:34 +0900

    post component 修正

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,41 +1,52 @@
 import { MoreVert } from '@mui/icons-material'
-import React from 'react'
+import React, { useState } from 'react'
 import './Post.css'
 import { Users } from '../../dummyData'
 
 
 function Post({ post }) {
+    const [like, setLike] = useState(post.like)
+    const [isLiked, setIsLiked] = useState(false)
+    const handleLike = () => {
+        setLike(isLiked ? like -1 : like + 1)
+        setIsLiked(!isLiked)
+    }
     return (
-    <div className='post'>
-        <div className='post-wrapper'>
-            <div className='post-top'>
-                <div className="post-top-left">
-                    <img src={Users.filter(user => user.id === post.id)[0].profilePicture} alt='' className='post-profile-img' />
-                    <span className='post-user-name'>
-                        {Users.filter(user => user.id === post.id)[0].username}
-                    </span>
-                    <span className='post-date'>{post.date}</span>
+        <div className='post'>
+            <div className='post-wrapper'>
+                <div className='post-top'>
+                    <div className="post-top-left">
+                        <img src={Users.filter(user => user.id === post.id)[0].profilePicture} alt='' className='post-profile-img' />
+                        <span className='post-user-name'>
+                            {Users.filter(user => user.id === post.id)[0].username}
+                        </span>
+                        <span className='post-date'>{post.date}</span>
+                    </div>
+                <div className='post-top-right'>
+                    <MoreVert />
                 </div>
-            <div className='post-top-right'>
-                <MoreVert />
-            </div>
-            </div>
-            <div className='post-center'>
-                <span className="post-text">{post.desc}</span>
-                <img src={post.photo} alt='' className='post-img' />
-            </div>
-            <div className="post-bottom">
-                <div className="post-bottom-left">
-                    <img src="/assets/heart.png" alt="" className='like-icon' />
-                    <span className="post-like-counter">{post.like}</span>
                 </div>
-                <div className="post-bottom-right">
-                    <span className="post-comment-text">{post.comment}</span>
+                <div className='post-center'>
+                    <span className="post-text">{post.desc}</span>
+                    <img src={post.photo} alt='' className='post-img' />
+                </div>
+                <div className="post-bottom">
+                    <div className="post-bottom-left">
+                        <img 
+                            src="/assets/heart.png" 
+                            alt="" 
+                            className='like-icon' 
+                            onClick={() => handleLike()}
+                        />
+                        <span className="post-like-counter">{like} いいね!</span>
+                    </div>
+                    <div className="post-bottom-right">
+                        <span className="post-comment-text">{post.comment}：comment</span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Post
