0c2f9c86c45d17692254bf474a05aac98712dbc8
ShotaMar <nu05kyntobronk@gmail.com>
2022-07-02 23:28:05 +0900

    ダミーデータ削除 DBから投稿を読み込ませるように修正

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,25 +1,38 @@
 import { MoreVert } from '@mui/icons-material'
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import './Post.css'
-import { Users } from '../../dummyData'
+// import { Users } from '../../dummyData'
 
 
 function Post({ post }) {
     const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER
-    const [like, setLike] = useState(post.like)
+    const [like, setLike] = useState(post.likes.length)
     const [isLiked, setIsLiked] = useState(false)
+    const [user, setUser] = useState({})
+
+    useEffect(() => { 
+        //useEfectにはasyncをつけられないため別でasync用関数を書く
+        const fetchUser = async() => {
+            const res = await axios.get(`/users/${post.userId}`)
+            setUser(res.data)
+        }
+        fetchUser()
+        },[])
+
     const handleLike = () => {
         setLike(isLiked ? like -1 : like + 1)
         setIsLiked(!isLiked)
     }
+
     return (
         <div className='post'>
             <div className='post-wrapper'>
                 <div className='post-top'>
                     <div className="post-top-left">
-                        <img src={PUBLIC_FOLDER + Users.filter(user => user.id === post.id)[0].profilePicture} alt='' className='post-profile-img' />
+                        <img src={user.profilePicture || PUBLIC_FOLDER + '/person/noAvatar.png'} alt='' className='post-profile-img' />
                         <span className='post-user-name'>
-                            {Users.filter(user => user.id === post.id)[0].username}
+                            {user.username}
                         </span>
                         <span className='post-date'>{post.date}</span>
                     </div>
@@ -29,7 +42,7 @@ function Post({ post }) {
                 </div>
                 <div className='post-center'>
                     <span className="post-text">{post.desc}</span>
-                    <img src={PUBLIC_FOLDER + post.photo} alt='' className='post-img' />
+                    <img src={PUBLIC_FOLDER + post.img} alt='' className='post-img' />
                 </div>
                 <div className="post-bottom">
                     <div className="post-bottom-left">
--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Post from '../post/Post.jsx'
 import Share from '../share/Share.jsx'
 import './Timeline.css'
-import { Posts } from '../../dummyData'
+import axios from "axios"
+// import { Posts } from '../../dummyData'
 
 function Timeline() {
+  const [ posts, setPosts ] = useState([])
+
+  useEffect(() => { 
+    //useEfectにはasyncをつけられないため別でasync用関数を書く
+    const fetchPosts = async() => {
+      const res = await axios.get('/posts/timeline/62aa05ceacb62a8278248741')
+      setPosts(res.data)
+    }
+    fetchPosts()
+  },[])
+
   return (
     <div className='timeline'>
         <div className='timeline-wrapper'>
             <Share />
-            {Posts.map(post => (
-                <Post post={post} key={post.id} />
+            {posts.map(post => (
+                <Post post={post} key={post._id} />
             ))}
         </div>
     </div>
