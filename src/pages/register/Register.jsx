2fc86352ead35186b6d23f9b96ca7301a42524e1
ShotaMar <nu05kyntobronk@gmail.com>
2022-07-03 17:15:18 +0900

    サインアップ実装

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './Register.css'
 
 function Register() {
+    const userName = useRef()
+    const email = useRef()
+    const password = useRef()
+    const passwordConfirmation = useRef()
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        //PWと確認用PWが同一かどうか確認
+        if(password.current.value != passwordConfirmation.current.value){
+            passwordConfirmation.current.setCustomValidity('パスワードが一致しません')
+        }else {
+            try {
+                const user = {
+                    username: userName.current.value,
+                    email: email.current.value,
+                    password: password.current.value
+                }
+                //registerAPIを叩く
+                await axios.post("/auth/register", user)
+                //ログイン画面へリダイレクト
+                navigate("/login")
+            }catch(err){
+                console.log(err)
+            }
+        }
+
+    }
+
     return (
         <div className="login">
             <div className="login-wrapper">
@@ -10,15 +41,15 @@ function Register() {
                     <span className="login-desc">SNS作成中</span>
                 </div>
                 <div className="login-right">
-                    <div className="login-box">
+                    <form className="login-box" onSubmit={(e) => handleSubmit(e)} >
                         <p className="login-message">create new account</p>
-                        <input type="text" className="login-input" placeholder='user name' />
-                        <input type="text" className="login-input" placeholder='mail' />
-                        <input type="text" className="login-input" placeholder='password'/>
-                        <input type="text" className="login-input" placeholder='again password'/>
-                        <button className="login-button">sign up</button>
+                        <input type="text" className="login-input" placeholder='user name' required ref={userName} />
+                        <input type="email" className="login-input" placeholder='email' required ref={email} />
+                        <input type="password" className="login-input" placeholder='password' required minLength="6" ref={password} />
+                        <input type="password" className="login-input" placeholder='again password' required minLength="6" ref={passwordConfirmation} />
+                        <button className="login-button" type='submit'>sign up</button>
                         <button className="login-register-button">login</button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>    
