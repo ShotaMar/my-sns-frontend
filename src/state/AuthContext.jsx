b2e03c215deeee5001a0158e9620cbe53fa4d4af
ShotaMar <nu05kyntobronk@gmail.com>
2022-07-03 17:55:13 +0900

    開発用に一旦initialStateを変更

--- src/state/AuthContext.jsx
+++ src/state/AuthContext.jsx
@@ -2,12 +2,27 @@ import { createContext, useReducer } from "react"
 import AuthReducer from "./AuthRecuder"
 
 //最初のユーザ状態を定義
+// const initialState = {
+//     user: null,
+//     isFetching: false,
+//     error: false
+// }
 const initialState = {
-    user: null,
+    user:{
+        _id: '62aa05ceacb62a8278248741',
+        username: "shota",
+        email: "shota@gmail.com",
+        password: "abcdefg",
+        profilePicture: "/assets/person/1.png",
+        coverPicture: "",
+        followers: [],
+        followings: [],
+        isAdmin: false,
+        desc: "React勉強中",
+    },
     isFetching: false,
     error: false
 }
-
 //状態をグローバルで管理する
 //createContextで作成したコンポーネントはProviderが使えるようになる
 export const AuthContext = createContext(initialState)
